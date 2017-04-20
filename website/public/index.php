168cce49876436ef790cfe7ae6156fc3d403df9f
Bm4n <D230966@edu.rocwb.nl>
2017-04-20 12:19:12 +0200

    website update

--- website/public/index.php
+++ website/public/index.php
@@ -12,10 +12,11 @@
 			<div class="par col-md-6 col-md-offset-3">
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam quisquam totam beatae suscipit ad quis provident expedita distinctio voluptate necessitatibus earum perferendis accusantium tempore labore, quo recusandae officia perspiciatis, ex? ipsum dolor sit amet, consectetur adipisicing elit. Similique, placeat? Ab non enim pariatur perferendis, minima, animi, corrupti quia quidem deleniti nihil dolore numquam? Quisquam quo assumenda vero, molestiae a.</p>
 			</div>
-			<form method="post" action="../app/registration.php" class="list-group col-md-6 col-md-offset-3">
+			<form method="post" action="../app/login.php" class="list-group col-md-6 col-md-offset-3">
 				<div class="input-group">
 					<input type="text" name="username" id="username" placeholder="username">
 					<input type="password" name="password" id="password" placeholder="password">
+					<input type="submit" name="submit" id="submit" value="login">
 				</div>
 			</form>
 			<div class="buffer"></div>
--- website/public/resultaten.php
+++ website/public/resultaten.php
@@ -10,9 +10,118 @@
 		</div>
 		<div class="main-content">
 			
-			
+			<!-- start content -->
+			<div class="container" id="overzichtinhoud">
+		        .resultaten
+		        <div class="tijdschema">
+		            <h3>Tijdschema</h3>
+		            <table width="90%">
+		                <tr>
+		                    <th>Team</th>
+		                    <th>Team</th>
+		                    <th>Tijd</th>
+		                </tr>
+		                <tr>
+		                    <th>Team 1</th>
+		                    <th>Team 2</th>
+		                    <th>10:00</th>
+		                </tr>
+		                <tr>
+		                    <th>Team 1</th>
+		                    <th>Team 2</th>
+		                    <th>10:00</th>
+		                </tr>
+		                <tr>
+		                    <th>Team 1</th>
+		                    <th>Team 2</th>
+		                    <th>10:00</th>
+		                </tr>
+		                <tr>
+		                    <th>Team 1</th>
+		                    <th>Team 2</th>
+		                    <th>10:00</th>
+		                </tr>
+		                <tr>
+		                    <th>Team 1</th>
+		                    <th>Team 2</th>
+		                    <th>10:00</th>
+		                </tr>
+		                <tr>
+		                    <th>Team 1</th>
+		                    <th>Team 2</th>
+		                    <th>10:00</th>
+		                </tr>
+		            </table>
+		        </div>
+		    </div>
+		    <div class="pouletitel">
+		        <h3>Poulestanden</h3>
+		    </div>
+		    <div class="poulestanden">
 
+		        <table width="90%">
+		            <h3>poule A</h3>
+		            <tr>
+		                <th>Team</th>
+		                <th>Score</th>
+		            </tr>
+		            <tr>
+		                <th>Team 1</th>
+		                <th>3</th>
+		            </tr>
+		            <tr>
+		                <th>Team 1</th>
+		                <th>3</th>
+		            </tr>
+		            <tr>
+		                <th>Team 1</th>
+		                <th>3</th>
+		            </tr>
+		            <tr>
+		                <th>Team 1</th>
+		                <th>3</th>
+		            </tr>
+		            <tr>
+		                <th>Team 1</th>
+		                <th>3</th>
+		            </tr>
+		        </table>
+		        <table width="90%">
+		            <h3>poule B</h3>
+		            <tr>
+		                <th>Team</th>
+		                <th>Score</th>
+		            </tr>
+		            <tr>
+		                <th>Team 1</th>
+		                <th>3</th>
+		            </tr>
+		            <tr>
+		                <th>Team 1</th>
+		                <th>3</th>
+		            </tr>
+		            <tr>
+		                <th>Team 1</th>
+		                <th>3</th>
+		            </tr>
+		            <tr>
+		                <th>Team 1</th>
+		                <th>3</th>
+		            </tr>
+		            <tr>
+		                <th>Team 1</th>
+		                <th>3</th>
+		            </tr>
+		        </table>
+		    </div>
+		    <div class="topscoorder">
+		        <h3><b>Topscoorder</b></h3>
+		        <h4>Team 3 Ik Natuurlijk</h4>
+		        <h3><b>Aantal goals</b></h3>
+		        <h2>6</h2>
+		    </div>
 			
+			<!-- stop content -->
 			<div class="buffer"></div>
 		</div>
     </div>
