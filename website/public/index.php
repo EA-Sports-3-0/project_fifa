f108b23228d7512d66f693882c56f841207daf4f
Bm4n <D230966@edu.rocwb.nl>
2017-04-20 11:38:19 +0200

    main.css and index.php fix

--- website/public/index.php
+++ website/public/index.php
@@ -1,10 +1,25 @@
-<?php require(realpath(__DIR__) . 'head.php'); ?>
+<?php require("head.php"); ?>
 
-    <div class="main-content">
-
-        <!-- site's main content goes here -->
-
-
+    <div class="container">
+		<div class="page-header">
+			<div class="title col-md-6 col-md-offset-3">
+				<h1>FIFA Dev Edition</h1>
+				<span>Login voor toegang</span>
+			</div>
+			<div class="buffer"></div>
+		</div>
+		<div class="main-content">
+			<div class="par col-md-6 col-md-offset-3">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam quisquam totam beatae suscipit ad quis provident expedita distinctio voluptate necessitatibus earum perferendis accusantium tempore labore, quo recusandae officia perspiciatis, ex? ipsum dolor sit amet, consectetur adipisicing elit. Similique, placeat? Ab non enim pariatur perferendis, minima, animi, corrupti quia quidem deleniti nihil dolore numquam? Quisquam quo assumenda vero, molestiae a.</p>
+			</div>
+			<form method="post" action="../app/registration.php" class="list-group col-md-6 col-md-offset-3">
+				<div class="input-group">
+					<input type="text" name="username" id="username" placeholder="username">
+					<input type="password" name="password" id="password" placeholder="password">
+				</div>
+			</form>
+			<div class="buffer"></div>
+		</div>
     </div>
 
-<?php require(realpath(__DIR__) . 'foot.php');
+<?php require("foot.php");
