2897d8cca733f5c725e4d85b1f887a35c87d03c2
d201670 <d201670@edu.rocwb.nl>
2017-04-21 11:09:03 +0200

    wireframesite en logboek

---
+++ website_createteam/website_noel/public/createteam.php
@@ -0,0 +1,124 @@
+<?php
+require_once ("header.php");
+?>
+
+<body>
+    <div class="container">
+        <header>
+            <div class="page-header">
+                <h1>FIFA Dev Edition</h1>
+                <div class="navbar-default"><a href="login.php">Login</a><a href="overzicht.php">Home</a><a href="register.php">Register</a><a
+                            href="resultaten.php">Resultaten</a></div>
+            </div>
+        </header>
+        <div class="container" id="createteaminhoud">
+            <div class="invoer">
+            <h3>Invoer</h3>
+                <div class="createteam">
+                    <div class="teamcolumn1">
+                    <h2>1</h2>
+                        <div class="teamname">
+                            <form action="">
+                                <p>Naam team</p>
+                                <input type="text">
+                            </form>
+                        </div>
+                        <div class="otherteams">
+                            <h4>Teams bekijken</h4>
+                            <?php 
+                            ?>
+                        </div>
+                    </div>
+                    <div class="teamcolumn2">
+                    <h2>2</h2>
+                        <form action="">
+                                    <p>Selecteer speler 1</p>
+                                    <select name="player" class="players">
+                                        <option value="speler">speler 1</option>
+                                        <option value="speler">speler 2</option>
+                                        <option value="speler">speler 3</option>
+                                        <option value="speler">speler 4</option>
+                                        <option value="speler">speler 5</option>
+                                        <option value="speler">speler 6</option>
+                                        <option value="speler">speler 7</option>
+                                        <option value="speler">speler 8</option>
+                                        <option value="speler">speler 9</option>
+                                        <option value="speler">speler 10</option>
+                                    </select>
+                                    <p>Selecteer speler 2</p>
+                                    <select name="player" class="players">
+                                        <option value="speler">speler 1</option>
+                                        <option value="speler">speler 2</option>
+                                        <option value="speler">speler 3</option>
+                                        <option value="speler">speler 4</option>
+                                        <option value="speler">speler 5</option>
+                                        <option value="speler">speler 6</option>
+                                        <option value="speler">speler 7</option>
+                                        <option value="speler">speler 8</option>
+                                        <option value="speler">speler 9</option>
+                                        <option value="speler">speler 10</option>
+                                    </select>
+                                    <p>Selecteer speler 3</p>
+                                    <select name="player" class="players">
+                                        <option value="speler">speler 1</option>
+                                        <option value="speler">speler 2</option>
+                                        <option value="speler">speler 3</option>
+                                        <option value="speler">speler 4</option>
+                                        <option value="speler">speler 5</option>
+                                        <option value="speler">speler 6</option>
+                                        <option value="speler">speler 7</option>
+                                        <option value="speler">speler 8</option>
+                                        <option value="speler">speler 9</option>
+                                        <option value="speler">speler 10</option>
+                                    </select>
+                                    <p>Selecteer speler 4</p>
+                                    <select name="player" class="players">
+                                        <option value="speler">speler 1</option>
+                                        <option value="speler">speler 2</option>
+                                        <option value="speler">speler 3</option>
+                                        <option value="speler">speler 4</option>
+                                        <option value="speler">speler 5</option>
+                                        <option value="speler">speler 6</option>
+                                        <option value="speler">speler 7</option>
+                                        <option value="speler">speler 8</option>
+                                        <option value="speler">speler 9</option>
+                                        <option value="speler">speler 10</option>
+                                    </select>
+                                    <p>Selecteer speler 5</p>
+                                    <select name="player" class="players">
+                                        <option value="speler">speler 1</option>
+                                        <option value="speler">speler 2</option>
+                                        <option value="speler">speler 3</option>
+                                        <option value="speler">speler 4</option>
+                                        <option value="speler">speler 5</option>
+                                        <option value="speler">speler 6</option>
+                                        <option value="speler">speler 7</option>
+                                        <option value="speler">speler 8</option>
+                                        <option value="speler">speler 9</option>
+                                        <option value="speler">speler 10</option>
+                                    </select>
+                                    <p>Selecteer reserve</p>
+                                    <select name="player" class="players">
+                                        <option value="speler">speler 1</option>
+                                        <option value="speler">speler 2</option>
+                                        <option value="speler">speler 3</option>
+                                        <option value="speler">speler 4</option>
+                                        <option value="speler">speler 5</option>
+                                        <option value="speler">speler 6</option>
+                                        <option value="speler">speler 7</option>
+                                        <option value="speler">speler 8</option>
+                                        <option value="speler">speler 9</option>
+                                        <option value="speler">speler 10</option>
+                                    </select>
+                        </form>
+                    </div>
+                    <div class="teamcolumn3">
+                    <h2>3</h2>
+                        <form action="">
+                            <input type="submit" value="Team invoer" class="buttonteaminvoer">
+                        </form>
+                    </div>
+            </div>
+           
+    </div>
+</body>
