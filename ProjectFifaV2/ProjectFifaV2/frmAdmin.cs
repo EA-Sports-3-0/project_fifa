c6eb9b20e61623724ff1b1c4d840a759ae837a70
d201670 <d201670@edu.rocwb.nl>
2017-05-10 16:52:24 +0200

    C# admin

--- ProjectFifaV2/ProjectFifaV2/frmAdmin.cs
+++ ProjectFifaV2/ProjectFifaV2/frmAdmin.cs
@@ -72,7 +72,16 @@ namespace ProjectFifaV2
             if (!(txtPath.Text == null))
             {
                 dbh.OpenConnectionToDB();
-
+                DialogResult result = MessageBox.Show("Are you sure you want to clear the database and add new data?", "Clear database", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                if (result.Equals(DialogResult.OK))
+                {
+                    DataTable hometable = dbh.FillDT("DELETE FROM [tblGames];");
+                    dbh.FillDT("DELETE FROM [tblPlayers];");
+                    dbh.FillDT("DELETE FROM [tblPredictions];");
+                    dbh.FillDT("DELETE FROM [tblTeams];");
+                    // Clear database
+                    // Update DB
+                }
                 dbh.CloseConnectionToDB();
             }
             else
